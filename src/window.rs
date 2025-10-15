f1c42f71ba970e6d767c07978969c7c50f6fca43
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-10-15 07:20:42 -0400

    basic rendering done

--- src/window.rs
+++ src/window.rs
@@ -4,26 +4,173 @@ use std::sync::Arc;
 use winit::event_loop::{self};
 use winit::{application::ApplicationHandler, event::{KeyEvent, WindowEvent}, event_loop::{ActiveEventLoop, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::Window};
 
+use wgpu::{wgt::TextureViewDescriptor, *};
+
 pub struct State {
+    surface: Surface<'static>,          // the render target essentially
+    device: Device,                     // the GPU
+    queue: Queue,                       // the work queue for submitting commands to the GPU
+    config: SurfaceConfiguration,       // the surface settings
+    is_surface_configured: bool,
     window: Arc<Window>
 }
 
 impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        let size = window.inner_size();
+
+        let instance_descriptor = 
+            InstanceDescriptor {
+                #[cfg(target_arch = "wasm32")]
+                backends: Backends::GL,
+                #[cfg(not(target_arch = "wasm32"))]
+                backends: Backends::PRIMARY,
+                ..Default::default()
+            };
+
+        let instance = Instance::new(
+            &instance_descriptor
+        );
+
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        // adapter is a handle for our graphics card
+        let adapter = instance.request_adapter( 
+            &RequestAdapterOptions { 
+                power_preference: PowerPreference::default(), 
+                force_fallback_adapter: false, 
+                compatible_surface: Some(&surface)
+            }
+        )
+        .await?;
+
+        println!("GPU: {}", adapter.get_info().name);
+
+        let (device, queue) = adapter.request_device(
+            &DeviceDescriptor { 
+                label: None, 
+                required_features: Features::empty(), 
+                required_limits: 
+                    if cfg!(target_arch = "wasm32") {
+                        Limits::downlevel_webgl2_defaults()
+                    } else {
+                        Limits::default()
+                    }
+                , 
+                memory_hints: Default::default(), 
+                trace: Trace::Off 
+            }
+        )
+        .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_fmt = surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_fmt,
+            
+            #[cfg(not(target_arch = "wasm32"))]
+            width: size.width,
+            #[cfg(not(target_arch = "wasm32"))]
+            height: size.height,
+
+            #[cfg(target_arch = "wasm32")]
+            width: size.width.min(2048),
+            #[cfg(target_arch = "wasm32")]
+            height: size.height.min(2048),
+
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+        
         Ok(Self {
-            window
+            surface,
+            window,
+            device,
+            queue,
+            config,
+            is_surface_configured: false
         })
     }
 
-    pub fn resize(&mut self, _width: u32, _height: u32) {
-        //temp
-        ()
+    pub fn resize(&mut self, width: u32, height: u32) {
+        if width > 0 && height > 0 {
+            #[cfg(not(target_arch = "wasm32"))]
+            {
+                self.config.width = width;
+                self.config.height = height;
+            }
+            #[cfg(target_arch = "wasm32")]
+            {
+                self.config.width = width.min(2048);
+                self.config.height = height.min(2048);
+            }
+            self.surface.configure(&self.device, &self.config);
+            self.is_surface_configured = true;
+        }
     }
 
-    pub fn render(&mut self) {
+    fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+        match (code, is_pressed) {
+            (KeyCode::Escape, true) => event_loop.exit(),
+            _ => ()
+        }
+    }
+
+    fn update(&mut self) {
+
+    }
+
+    pub fn render(&mut self) -> Result<(), SurfaceError>{
         self.window.request_redraw();
 
-        // more
+        if !self.is_surface_configured {
+            return Ok(());
+        }
+
+        let output = self.surface.get_current_texture()?;
+
+        let view = output.texture.create_view(&TextureViewDescriptor::default());
+
+        let mut encoder = self.device.create_command_encoder( &CommandEncoderDescriptor {
+            label: Some("Render Encoder")
+        });
+
+        {
+            let _render_pass = encoder.begin_render_pass(
+                &RenderPassDescriptor { 
+                    label: Some("Render Pass"), 
+                    color_attachments: &[Some(
+                        RenderPassColorAttachment { 
+                            view: &view, 
+                            resolve_target: None, 
+                            ops: Operations { 
+                                load: LoadOp::Clear(
+                                    Color { r: 0.1, g: 0.2, b: 0.3, a: 1.0 }
+                                ), 
+                                store: StoreOp::Store
+                            },
+                            depth_slice: None, 
+                        }
+                    )], 
+                    depth_stencil_attachment: None, 
+                    timestamp_writes: None, 
+                    occlusion_query_set: None 
+                }
+            );
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
 
@@ -122,7 +269,17 @@ impl ApplicationHandler<State> for App {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::Resized(size) => state.resize(size.width, size.height),
             WindowEvent::RedrawRequested => {
-                state.render();
+                state.update();
+                match state.render() {
+                    Ok(_) => (),
+                    Err(SurfaceError::Lost | SurfaceError::Outdated) => {
+                        let size = state.window.inner_size();
+                        state.resize(size.width, size.height);
+                    },
+                    Err(e) => {
+                        log::error!("Unable to render {}", e);
+                    }
+                }
             }
             WindowEvent::KeyboardInput { 
                 event: KeyEvent {
@@ -131,10 +288,7 @@ impl ApplicationHandler<State> for App {
                         ..
                     },
                 ..
-            } => match (code, key_state.is_pressed()) {
-                (KeyCode::Escape, true) => event_loop.exit(),
-                _ => ()
-            },
+            } => state.handle_key(&event_loop, code, key_state.is_pressed()),
             _ => ()
         }
     }
