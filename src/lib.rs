3addb6fea65495e3dbc44eeeff8e61a201afebdf
Saahil Gupta <55856655+seabiscuit-iv@users.noreply.github.com>
2025-10-15 06:19:04 -0400

    window setup finished

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod window;
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn run_web() -> Result<(), wasm_bindgen::JsValue> {
+    use window::*;
+    use wasm_bindgen::UnwrapThrowExt;
+
+
+    console_error_panic_hook::set_once();
+    run().unwrap_throw();
+
+    Ok(())
+}
